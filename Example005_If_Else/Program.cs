c3df39b273ce2b4321e6968a8da268b3cbb575ec
Paul Gre <pgrechihin@mail.ru>
2023-02-09 21:13:57 +0300

    Example 005 Paul

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine(username);
+ 
+ if(username.ToLower() == "paul")
+ {
+    Console.WriteLine("Да это же Павел!");
+ }
+ else
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
